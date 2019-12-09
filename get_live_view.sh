261d26c55d04ac95ff2276b5a513edd8db6e7a53
Elvis on Raspberry <elvis@elvisun.com>
2019-12-09 15:26:47 -0500

    change live image location

--- get_live_view.sh
+++ get_live_view.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-fswebcam -S 30 -r 480x240 --no-banner /home/pi/github/garage-monitoring/live.jpg
+fswebcam -S 30 -r 480x240 --no-banner /home/pi/Documents/github/garage-monitoring/live.jpg
